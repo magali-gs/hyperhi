66f80a680f38defdae616109c933f81734ec1abb
Magali's Personal <magali.goncalvess@gmail.com>
2024-12-13 11:31:39 -0300

    Add canvas element and implement drawing functionality with custom cursor

--- src/script.js
+++ src/script.js
@@ -1,18 +1,66 @@
 const cursor = document.querySelector('.cursor');
+const canvasTag = document.querySelector('canvas.in');
+let isMouseDown = false;
 
 const growCursor = () => {
-    cursor.classList.add('is-down');
+  cursor.classList.add('is-down');
 };
 
 const shrinkCursor = () => {
-    cursor.classList.remove('is-down');
+  cursor.classList.remove('is-down');
 };
 
 const moveCursor = (x, y) => {
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
 };
 
-document.addEventListener('mousedown', growCursor);
-document.addEventListener('mouseup', shrinkCursor);
-document.addEventListener('mousemove', (e) => moveCursor(e.pageX, e.pageY));
+const setupCanvas = (canvas) => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+  const dpi = window.devicePixelRatio;
+  const context = canvas.getContext('2d');
+
+  canvas.width = w * dpi;
+  canvas.height = h * dpi;
+  canvas.style.width = w + 'px';
+  canvas.style.height = h + 'px';
+
+  context.scale(dpi, dpi);
+  context.strokeStyle = 'red';
+  context.lineWidth = 80;
+  context.lineCap = 'round';
+  context.lineJoin = 'round';
+}
+
+const startToDraw = (canvas, x, y) => {
+  const context = canvas.getContext('2d');
+  context.moveTo(x, y); 
+}
+
+const moveDraw = (canvas, x, y) => {
+  const context = canvas.getContext('2d');
+  if (isMouseDown) {
+    context.lineTo(x, y);
+    context.stroke();
+  }
+}
+
+setupCanvas(canvasTag);
+
+document.addEventListener('mousedown', (e) => { 
+  isMouseDown = true;
+  
+  growCursor();
+  startToDraw(canvasTag, e.pageX, e.pageY);
+});
+
+document.addEventListener('mouseup', () => {
+  isMouseDown = false;
+  shrinkCursor()
+});
+
+document.addEventListener('mousemove', (e) => {
+  moveCursor(e.pageX, e.pageY);
+  moveDraw(canvasTag, e.pageX, e.pageY);
+});
